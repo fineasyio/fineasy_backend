e25ad8157cb1ffefe436d22cdf217f2dde74a1a1
Natan Nascimento <natan.nascimento@xpi.com.br>
2022-03-23 23:40:10 -0300

    feat: added base api with healthcheck endpoint

---
+++ api/routes/healthcheck/__init__.py
@@ -0,0 +1,10 @@
+from api.routes.factory import RouterFactory
+
+router = RouterFactory(version="v1", tag="healthcheck").get
+
+
+@router.post("/healthcheck",
+             response_description="API Healthcheck",
+             status_code=200)
+def healthcheck():
+    return {"status": "ok"}
