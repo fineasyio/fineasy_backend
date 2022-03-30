c3f631ea1a166698005c8033447a079ab1e96218
Natan Nascimento <natan.nascimento@xpi.com.br>
2022-03-30 22:18:10 +0000

    feat: added database repository (#12)

---
+++ api/infrastructure/repositories/database/__init__.py
@@ -0,0 +1,7 @@
+from api.infrastructure.repositories.database.producer import DataProducer
+from api.infrastructure.repositories.database.connection import DatabaseConnection
+
+
+__all__ = [
+    "DataProducer", "DatabaseConnection"
+]
